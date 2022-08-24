2ea37ed507b6878c60161192def949f332a24089
alfred <839114222@qq.com>
2022-08-24 16:31:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 import pytest
 from common.utils import Utils
-from common.utils import project_dir
 from common.logger import logger
 import os
+from config.common_config import config
 
 
 def main():
-    test_data_path = project_dir + "/testData"
+    test_data_path = config.get("test_data_path")
     logger.info(f"--测试数据的路径是{test_data_path}")
     logger.info("--读取测试yml文件---")
     test_data = Utils.get_test_data(test_data_path)
     logger.info(f"读取的yml文件列表是\n{str(test_data)}")
-    out_put_dir = project_dir + "/testCases"
+    out_put_dir = config.get("out_put_dir")
     logger.info(f"测试用例目录是{out_put_dir}")
     logger.info(f"先删除之前生成的pytest脚本")
     Utils.remove_old_script(out_put_dir)
@@ -27,8 +27,10 @@ def main():
         map(Utils.beautify_test_case_file, [os.path.join(root, file) for file in files if file.startswith("test")])
     logger.info(f"格式化测试脚本完成……")
     logger.info(f"pytest脚本生成完成")
+    logger.info(f"开始执行pytest测试脚本")
+    pytest.main()
+    logger.info(f"pytest测试脚本执行完成")
 
 
 if __name__ == "__main__":
     main()
-    # pytest.main()
